a202148bc76810b758bab8f5c01f45b40bdc172c
BobbyX208 <ronaldbob365@gmail.com>
2025-02-09 23:25:07 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = "YOUR_BOT_TOKEN"
-ADMIN_ID = 123456789  # Replace with your Telegram ID
+BOT_TOKEN = "7738437790:AAG4eUFBtvtGp0C-npwpZGwEcFjvY2fw9Qo"
+ADMIN_ID = 6127276522  # Replace with your Telegram ID
 WEBHOOK_URL = "https://your-koyeb-app-url.koyeb.app"  # Replace with your Koyeb app URL
